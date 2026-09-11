5d95da5175eb56b1d53f4d1d67c882bbbdfd3f56
Cristian Matiut <cmatiut@cloudbasesolutions.com>
2026-09-11 10:55:34 +0300

    Add relist for stackit source project option

--- config.ts
+++ config.ts
@@ -143,6 +143,12 @@ const conf: Config = {
     },
     {
       name: "stackit",
+      types: ["source"],
+      requiredFields: [],
+      relistFields: ["project"],
+    },
+    {
+      name: "stackit",
       types: ["destination"],
       requiredFields: ["availability_zone"],
       relistFields: ["project"],
